ef368641ecd6e5930e955d2201ec056a095d6b49
Aditya1312004 <144265726+Aditya1312004@users.noreply.github.com>
2025-08-10 22:59:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (45.74%) - LeetSync

---
+++ 2602-maximum-enemy-forts-that-can-be-captured/maximum-enemy-forts-that-can-be-captured.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int captureForts(int[] forts) {
+        int j=0;
+        int max= 0;
+
+        for(int i=0;i<forts.length;i++){
+            if(forts[i] != 0){
+                if(forts[j] == - forts[i] ){
+                    max = Math.max(max , i-j-1);
+                }
+                 j= i;
+            }
+           
+        }
+        return max;
+    }
+}
