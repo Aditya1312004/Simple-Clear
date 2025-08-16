ce2afca70d1f8fa09b13888b13fccaf91465d844
Aditya1312004 <144265726+Aditya1312004@users.noreply.github.com>
2025-08-16 12:40:13 +0530

    Time: 3 ms (13.50%) | Memory: 40.6 MB (44.60%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,21 +1,8 @@
 class Solution {
     public int maximum69Number (int num) {
-        List<Integer> as = new ArrayList<>();
-        while(num>0){
-            as.add(0,num%10);
-            num=num/10;
-        }
+        String s = String.valueOf(num);
 
-        for(int i=0;i<as.size();i++){
-            if(as.get(i) == 6){
-                as.set(i , 9);
-                break;
-            }
-        }
-        int result = 0;
-        for(int d : as){
-            result = result*10 + d;
-        }
-        return result;
+        s = s.replaceFirst("6" , "9");
+        return Integer.parseInt(s);
     }
 }
