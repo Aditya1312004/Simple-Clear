b99480d55fba6c14d9e83652db4dc078759b6d31
Aditya1312004 <144265726+Aditya1312004@users.noreply.github.com>
2025-08-26 01:29:19 +0530

    Time: 900 ms (25.25%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,5 +1,6 @@
-SELECT contest_id, 
-       ROUND(COUNT(user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
-FROM Register
-GROUP BY contest_id
-ORDER BY percentage DESC, contest_id;
+/* Write your PL/SQL query statement below */
+select r.contest_id  , 
+ROUND(COUNT(DISTINCT r.user_id)* 100 / (select COUNT(*) from Users) , 2) AS percentage
+FROM Register r
+GROUP BY r.contest_id 
+ORDER BY percentage DESC,r.contest_id ASC  
